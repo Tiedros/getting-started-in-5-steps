eb9fd32a6adbad2592b141a4330e8077927f9509
Tiedros Abrha <tiedrosabrha@gmail.com>
2018-02-16 00:32:59 -0600

    Simple message for ssh communicaition

--- eclipse-in-5-steps/src/HelloWorld.java
+++ eclipse-in-5-steps/src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		Map<String, String> map = new HashMap<String, String>();
 		map.put("key", "value");
+		//adding comment for SSH communication based on password
 		System.out.println("This is added for git pull purpose!!!!");
 		System.out.println(">>>>from SSH This is added for git Push purpose!!!!);
 	}
