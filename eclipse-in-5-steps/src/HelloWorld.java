03aa26425c859d17fae9a1f52eb89fd906cb95d6
Tiedros Abrha <tiedrosabrha@gmail.com>
2018-02-16 00:21:12 -0600

    Sample message for demonstrating ssh communication

--- eclipse-in-5-steps/src/HelloWorld.java
+++ eclipse-in-5-steps/src/HelloWorld.java
@@ -6,6 +6,6 @@ public class HelloWorld {
 		Map<String, String> map = new HashMap<String, String>();
 		map.put("key", "value");
 		System.out.println("This is added for git pull purpose!!!!");
-		System.out.println("This is added for git Push purpose!!!!);
+		System.out.println(">>>>from SSH This is added for git Push purpose!!!!);
 	}
 }
