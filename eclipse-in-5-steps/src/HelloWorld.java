c6e219609aa124f22ba8f160d4de55f3d9960eb8
Tiedros Abrha <36466860+Tiedros@users.noreply.github.com>
2018-02-15 23:17:22 -0600

    update for git purpose

--- eclipse-in-5-steps/src/HelloWorld.java
+++ eclipse-in-5-steps/src/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		Map<String, String> map = new HashMap<String, String>();
 		map.put("key", "value");
+		System.out.println("This is added for git pull purpose");
 	}
 }
